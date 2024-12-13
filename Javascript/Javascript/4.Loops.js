8177a49a8e17f3289ddcc9dde89a5e7ad9ebc60c
Babita00 <babitaasth07@gmail.com>
2024-12-13 12:35:48 +0545

    formatting

--- Javascript/Javascript/4.Loops.js
+++ Javascript/Javascript/4.Loops.js
@@ -10,46 +10,45 @@
 // }
 
 let obj = {
-    name: "Babita",
-    role: "developer",
-    company: "abc"
-}
+  name: "Babita",
+  role: "developer",
+  company: "abc",
+};
 for (const key in obj) {
-
-    const element = obj[key];
-    console.log(element);
-    console.log(key);
-    console.log(obj);
-    console.log(key, obj);
-
+  const element = obj[key];
+  console.log(element);
+  console.log(key);
+  console.log(obj);
+  console.log(key, obj);
 }
+
 for (const c of "babita") {
-    console.log(c);
+  console.log(c);
 }
 console.log("while loop");
 let i = 0;
 while (i < 5) {
-    console.log(i);
-    i++;
+  console.log(i);
+  i++;
 }
 console.log("for-of-loop");
 let str = "Babita";
 let size = 0;
 for (let i of str) {
-    console.log(i);
-    size++;
+  console.log(i);
+  size++;
 }
 console.log("size=", size);
 console.log("For in loop");
 const Product = {
-    Name: "Ball Pen",
-    color: "Black",
-    rating: 4,
-    isDeal: true,
-    price: 270,
-    offer: 5
+  Name: "Ball Pen",
+  color: "Black",
+  rating: 4,
+  isDeal: true,
+  price: 270,
+  offer: 5,
 };
 
 for (let key in Product) {
-    console.log(key, "=", Product[key]);
+  console.log(key, "=", Product[key]);
 }
